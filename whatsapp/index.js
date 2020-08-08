2765d442a1b5fcb29faff8c3b6dae8975ad432a4
Fabio Dias <mfabiodias@gmail.com>
2020-08-08 19:09:01 -0300

    Fix Bug bot.json

--- whatsapp/index.js
+++ whatsapp/index.js
@@ -8,7 +8,7 @@ var path = require("path");
 var argv = require('yargs').argv;
 var rev = require("./detectRev");
 var constants = require("./constants");
-var configs = require("../bot");
+var configs = require("./bot/bot");
 var qrcode = require('qrcode');
 const config = require('../config/config');
 
